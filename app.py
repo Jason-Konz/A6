6a70fe16e950cded1d04a0ebd257d00aa7b1dc2d
SamBardwell <50633020+SamBardwell@users.noreply.github.com>
2023-04-16 16:14:25 -0500

    Not working

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
 )
 from flask_sqlalchemy import SQLAlchemy
 import os 
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = b'REPLACE_ME_x#pi*CO0@^z_beep_beep_boop_boop'
@@ -35,7 +36,7 @@ def authenticate(username, password):
     return False
 
 def is_secure_route(request):
-    return request.path not in ['/login/', '/logout/', '/profile/'] and \
+    return request.path not in ['/login/', '/logout/', '/profile/', '/profile/new/'] and \
         not request.path.startswith('/static/')
 
 @app.before_request
@@ -83,9 +84,37 @@ def logout():
 
 
 @app.route('/profile/new/', methods=["GET"])
-def profile():
-    pass
+def new_profile_form():
+    return render_template('new_profile_form.html')
 
 @app.route('/profile/', methods=["POST"])
 def profile():
-    pass
+    print("Made it")
+    new_username = request.form["username"]
+    new_password = request.form["password"]
+    new_email = request.form["email"]
+    infile = request.files["img"]
+    infile.filename = new_username + infile.filename
+    filename = secure_filename(infile)
+    filepath = os.path.join(IMAGE_DIR, filename)
+
+    print("Made it 1")
+    if '' in [new_username, new_password, new_email] or not infile:
+        return redirect(url_for("new_profile_form"), \
+            messages=['Please fill in all fields'])
+
+    print("Made it 2")
+    user = Profile.query.filter_by(username=new_username).first()
+    print("Made it 3")
+    if user:
+        return redirect(url_for("new_profile_form"), \
+            messages=[f'{new_username} is already taken'])
+    print("Made it 4")
+    infile.save(filepath)
+    newprofile = Profile(username=new_username, password=new_password, \
+                         email=new_email, photofn=filename, posts=None)
+    print("Made it 5")
+    db.session.add(newprofile)
+    db.session.commit()
+    print("Made it 6")
+    return redirect(url_for("login_form"))
